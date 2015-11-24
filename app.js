5f095b99aa505230111665d935b1b15a6ff5ce93
Lancey6 <david.thomas.reeve@gmail.com>
2015-11-24 13:50:49 -0500

    Finished styling

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    sass = require('node-sass-middleware'),
     swig = require('swig'),
     bodyParser = require('body-parser'),
     morgan = require('morgan'),
@@ -6,11 +7,26 @@ var express = require('express'),
 
 var app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+swig.setDefaults({ cache: false });
+
 app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use( sass( {
+  src: __dirname + '/assets',
+  dest: __dirname + '/public',
+  debug: true,
+}));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, function(err){
   console.log('Listening to port 3000');
 });
+
+app.get('/', function( req, res, next ) {
+  res.render( 'layout' );
+} );
